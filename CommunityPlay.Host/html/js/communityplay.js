d59166cf8af8e0cea1b1cafc61b2c21b8a002f97
Robert Chipperfield <robert@syxis.co.uk>
2013-11-14 23:16:49 +0000

    Starting to make interface look a bit prettier...

--- CommunityPlay.Host/html/js/communityplay.js
+++ CommunityPlay.Host/html/js/communityplay.js
@@ -3,6 +3,15 @@
         for (var i = 0; i < data.length; i++) {
             var media = data[i];
             $("#medialist").append("<li><a href=\"/api/audio/play/" + media.ID + "\">" + media.Name + "</a></li>");
+
+            var cartDiv = $("<div class=\"col-md-4\"></div>");
+            var button = $("<button />").text(media.Name);
+            button.click(function () {
+                var id = media.ID;
+                $.get("/api/audio/play/" + id);
+            });
+            cartDiv.append(button);
+            $("#cartsrow").append(cartDiv);
         }
     });
 });

fa2bb4c42f804abc660dee3b3be1ae06872042f6
Robert Chipperfield <robert@syxis.co.uk>
2013-11-15 00:06:40 +0000

    Change to some Knockout tastiness

--- CommunityPlay.Host/html/js/communityplay.js
+++ CommunityPlay.Host/html/js/communityplay.js
@@ -1,17 +1,26 @@
 ﻿$(function () {
+    var viewModel = {
+        title: ko.observable("CommunityPlay @ Cambridge 105"),
+        searchTerm: ko.observable("Search..."),
+        mediaResults: ko.observableArray(),
+        cartButtons: ko.observableArray()
+    };
+
+    ko.applyBindings(viewModel);
+
     $.getJSON("/api/medialibrary/list", function (data) {
         for (var i = 0; i < data.length; i++) {
             var media = data[i];
-            $("#medialist").append("<li><a href=\"/api/audio/play/" + media.ID + "\">" + media.Name + "</a></li>");
-
-            var cartDiv = $("<div class=\"col-md-4\"></div>");
-            var button = $("<button />").text(media.Name);
-            button.click(function () {
-                var id = media.ID;
-                $.get("/api/audio/play/" + id);
-            });
-            cartDiv.append(button);
-            $("#cartsrow").append(cartDiv);
+            var mediaItem = {
+                text: media.Name,
+                play: function() { 
+                    var id = media.ID;
+                    return function () {                    
+                        $.get("/api/audio/play/" + id);
+                }}()
+            };
+            viewModel.mediaResults.push(mediaItem);
+            viewModel.cartButtons.push(mediaItem);
         }
     });
 });
